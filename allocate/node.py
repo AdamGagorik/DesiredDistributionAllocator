a90fbfd26e79b8ee335e87e106afa047fa0a0b43
Adam Gagorik <adam.gagorik@gmail.com>
2020-11-19 17:30:01 -0600

    Add node column types

--- allocate/node.py
+++ allocate/node.py
@@ -2,10 +2,11 @@
 The data associated with each node or column when representing the problem.
 """
 import dataclasses
+import typing
 
 
 @dataclasses.dataclass()
-class COLUMNS:
+class NodeColumns:
     """
     The columns in a table representation.
     """
@@ -15,6 +16,49 @@ class COLUMNS:
     current_value: str = 'current_value'
     current_ratio: str = 'current_ratio'
     update_amount: str = 'update_amount'
+
+    @classmethod
+    def values(cls, *columns, strict: bool = False):
+        """
+        A list of column types.
+        """
+        for f in dataclasses.fields(cls):
+            if columns:
+                if f.name in columns:
+                    yield f.default
+                else:
+                    if strict:
+                        raise ValueError(f'unknown column: {f.name}')
+            else:
+                yield f.default
+
+
+@dataclasses.dataclass()
+class NodeColumnTypes:
+    """
+    The columns in a table representation.
+    """
+    label: typing.Callable = str
+    level: typing.Callable = int
+    desired_ratio: typing.Callable = float
+    current_value: typing.Callable = float
+    current_ratio: typing.Callable = float
+    update_amount: typing.Callable = float
+
+    @classmethod
+    def dtypes(cls, *columns, strict: bool = False):
+        """
+        A mapping from column names to types.
+        """
+        for f in dataclasses.fields(cls):
+            if columns:
+                if f.name in columns:
+                    yield f.name, f.default
+                else:
+                    if strict:
+                        raise ValueError(f'unknown column: {f.name}')
+            else:
+                yield f.name, f.default
 
 
 @dataclasses.dataclass()
