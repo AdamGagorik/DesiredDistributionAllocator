78f757f8d8a12d664cf783f3930e2d5b9b9211cc
Adam Gagorik <adam.gagorik@gmail.com>
2020-11-24 13:30:25 -0600

    Refactor graph solver so that distribute happens even with no amount added

--- allocate/solvers/graphsolver.py
+++ allocate/solvers/graphsolver.py
@@ -16,6 +16,7 @@ A    level=[0] results_value=[ 5,000.00] results_ratio=[1.000] amount_to_add=[  
  └─2 level=[1] results_value=[ 1,250.00] results_ratio=[0.250] amount_to_add=[   250.00]
 """
 import networkx as nx
+import typing
 import copy
 
 from allocate.solvers.constrained import BucketSolverConstrained
@@ -26,7 +27,7 @@ import allocate.network.algorithms
 
 
 def solve(graph: nx.DiGraph, solver: BucketSolver = BucketSolverConstrained,
-          inplace: bool = False, max_attempts: int = 1024, **kwargs) -> nx.DiGraph:
+          inplace: bool = False, max_attempts: int = 10, **kwargs) -> nx.DiGraph:
     """
     Solve the bucket problem over a hierarchy of buckets.
 
@@ -43,46 +44,88 @@ def solve(graph: nx.DiGraph, solver: BucketSolver = BucketSolverConstrained,
     if not inplace:
         graph = copy.deepcopy(graph)
 
-    source = allocate.network.algorithms.get_graph_root(graph)
+    # applying the solver here allows initial redistribution for unconstrained solvers
+    _apply_solver_over_graph(graph, solver, lambda a: a >= 0, **kwargs)
     for attempt in range(max_attempts):
-        stop_algorithm = True
-        # walk the graph from the bottom up, solving the bucket problem set of children
-        for parent, children in reversed(list(nx.bfs_successors(graph, source))):
-            amount_to_add = graph.nodes[parent][node_attrs.amount_to_add.column]
-            if amount_to_add > 0:
-                current_values = [graph.nodes[n][node_attrs.current_value.column] for n in children]
-                optimal_ratios = [graph.nodes[n][node_attrs.optimal_ratio.column] for n in children]
-                stop_algorithm = False
-                # solve the bucket problem over the children
-                system = allocate.solvers.BucketSystem.create(
-                    amount_to_add=amount_to_add, current_values=current_values,
-                    optimal_ratios=optimal_ratios, labels=children)
-                solved = solver.solve(system, **kwargs)
-                # negate the amount to add so we dont try to add it again on the next pass
-                graph.nodes[parent][node_attrs.amount_to_add.column] = -amount_to_add
-                # increment the amount to add value for the children of this node
-                for i, child in enumerate(children):
-                    graph.nodes[child][node_attrs.amount_to_add.column] += solved.result_delta.values[i]
+        stop_algorithm = _apply_solver_over_graph(graph, solver, lambda a: a > 0, **kwargs)
         if stop_algorithm:
             break
     else:
         raise RuntimeError('max attempts reached in network solver!')
 
+    graph = _finalize_graph(graph)
+    return graph
+
+
+def _apply_solver_over_graph(graph: nx.DiGraph, solver: BucketSolver, condition: typing.Callable, **kwargs) -> bool:
+    """
+    Walk the graph from the bottom up, solving the bucket problem over the set of children for each parent.
+
+    Parameters:
+        graph: The DAG to process.
+        solver: The bucket solver during traversal.
+        condition: The continue condition to apply on the amount to add.
+        **kwargs: Extra key word arguments to the solver's solve method.
+
+    Returns:
+        True if the continue condition was never met.
+    """
+    stop_algorithm = True
+    # walk the graph from the bottom up, solving the bucket problem set of children
+    source = allocate.network.algorithms.get_graph_root(graph)
+    for parent, children in reversed(list(nx.bfs_successors(graph, source))):
+        amount_to_add = graph.nodes[parent][node_attrs.amount_to_add.column]
+
+        if condition(amount_to_add):
+            current_values = [graph.nodes[n][node_attrs.current_value.column] for n in children]
+            optimal_ratios = [graph.nodes[n][node_attrs.optimal_ratio.column] for n in children]
+            stop_algorithm = False
+
+            # solve the bucket problem over the children
+            system = allocate.solvers.BucketSystem.create(
+                amount_to_add=amount_to_add, current_values=current_values,
+                optimal_ratios=optimal_ratios, labels=children)
+            solved = solver.solve(system, **kwargs)
+
+            # negate the amount to add so we dont try to add it again on the next pass
+            graph.nodes[parent][node_attrs.amount_to_add.column] = -amount_to_add
+
+            # increment the amount to add value for the children of this node
+            for i, child in enumerate(children):
+                graph.nodes[child][node_attrs.amount_to_add.column] += solved.result_delta.values[i]
+
+    return stop_algorithm
+
+
+def _finalize_graph(graph: nx.DiGraph) -> nx.DiGraph:
+    """
+    Finalize the amount_to_add, results_value, and results_ratio column for the graph.
+
+    Parameters:
+        graph: The DAG to process.
+
+    Returns:
+        The processed DAG.
+    """
     # finalize the amounts to add and the results_value column
     for node in graph:
-        graph.nodes[node][node_attrs.amount_to_add.column] = abs(graph.nodes[node][node_attrs.amount_to_add.column])
-        graph.nodes[node][node_attrs.results_value.column] = \
-            graph.nodes[node][node_attrs.current_value.column] + \
-            graph.nodes[node][node_attrs.amount_to_add.column]
-
         # noinspection PyCallingNonCallable
         if graph.out_degree(node):
             graph.nodes[node][node_attrs.amount_to_add.column] = 0
+        else:
+            graph.nodes[node][node_attrs.results_value.column] = \
+                graph.nodes[node][node_attrs.current_value.column] + \
+                graph.nodes[node][node_attrs.amount_to_add.column]
+
+    source = allocate.network.algorithms.get_graph_root(graph)
+    for parent, children in reversed(list(nx.bfs_successors(graph, source))):
+        graph.nodes[parent][node_attrs.results_value.column] = sum(
+            graph.nodes[child][node_attrs.results_value.column] for child in children)
 
     # calculate the final ratios for the results column
     graph = allocate.network.algorithms.normalize(
         graph, inplace=True,
-        key=allocate.network.attributes.node_attrs.results_value.column,
-        out=allocate.network.attributes.node_attrs.results_ratio.column)
+        key=node_attrs.results_value.column,
+        out=node_attrs.results_ratio.column)
 
     return graph
